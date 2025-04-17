8f984452370395d38db32b639aaee8e04bad5d11
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-04-17 09:03:07 +0530

    Add files via upload

---
+++ Main (4).java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String args[] ){
+        String data = "Piyush";
+        String data1 = "Mohit";
+        if(data.equals(data1)){
+            System.out.println("Both are not equal to");
+        }
+        else{
+            System.out.println("Both are not equal to");
+        }
+    }
+    
+}
